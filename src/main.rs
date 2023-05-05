dfb4f331c8d42f10a5b849fb9d61ff04b8735aae
Dennis Jensen <ngh1adj95@hotmail.com>
2023-05-05 18:42:10 +0200

    Feature: Now counting line coverage on a new commit
    Changes:
    - Line coverage on a new commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use patch::Patch;
 use serde::Deserialize;
 
@@ -79,16 +80,6 @@ struct Line {
 
 // Do an implementation of the Package
 impl Package {
-    fn print(&self) {
-        println!("package: name: {:#?}", self.name);
-        println!("package: line rate: {:#?}", self.line_rate);
-        println!("package: branch rate: {:#?}", self.branch_rate);
-        println!("package: complexity: {:#?}", self.complexity);
-
-        println!("Filename: {:#?}", self.classes.class.filename);
-        println!("Lines covered: {:#?}", self.get_lines_covered());
-    }
-
     fn get_filepath(&self) -> String {
         self.classes.class.filename.clone()
     }
@@ -109,6 +100,25 @@ impl Package {
     }
 }
 
+impl Coverage {
+    fn new(file_string: &str) -> Coverage {
+        let coverage: Coverage = serde_xml_rs::from_str(&file_string).unwrap();
+        coverage
+    }
+
+    fn get_lines_covered(&self, file_path: &str) -> Vec<usize> {
+        let mut lines_covered: Vec<usize> = Vec::new();
+
+        for package in &self.packages.list_of_packages {
+            if package.get_filepath() == file_path {
+                lines_covered = package.get_lines_covered();
+            }
+        }
+
+        lines_covered
+    }
+}
+
 // Make a function that runs a system command
 fn run_command(command: &str) -> String {
     let output = std::process::Command::new("bash")
@@ -121,31 +131,122 @@ fn run_command(command: &str) -> String {
     stdout
 }
 
-fn main() {
-    // Read xml file
-    let file_string = std::fs::read_to_string("coverage.xml").unwrap();
-    let diff_file_string = run_command(command: "git diff main");
+struct DiffFiles {
+    files: Vec<(String, Vec<usize>)>,
+}
 
-    let patches = match Patch::from_multiple(&diff_file_string) {
-        Ok(p) => p,
-        Err(e) => panic!("Error: {}", e),
+impl DiffFiles {
+    fn new(diff_file_string: &str) -> DiffFiles {
+        // Parse the diff file and return file paths and line numbers changed
+        let patches = match Patch::from_multiple(&diff_file_string) {
+            Ok(p) => p,
+            Err(e) => panic!("Error parsing diff file: {}", e),
+        };
+
+        let mut files_changed = DiffFiles { files: Vec::new() };
+
+        for patch in patches {
+            let file_path = patch.new.path;
+
+            let mut lines_changed: Vec<usize> = Vec::new();
+
+            for hunk in patch.hunks {
+                let line_range = hunk.new_range;
+                for line_number in line_range.start..line_range.start + line_range.count {
+                    lines_changed.push(line_number as usize);
+                }
+            }
+
+            // Remove b/ from the start of the file path
+            let file_path = file_path[2..].to_string();
+
+            files_changed.files.push((file_path.into(), lines_changed));
+        }
+
+        files_changed
+    }
+}
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Coverage file to use
+    #[arg(short, long)]
+    coverage_file: String,
+
+    /// Branch to compare against
+    #[arg(short, long)]
+    branch: String,
+
+    /// Fail if coverage is below this threshold
+    #[arg(short, long, default_value = "0.0")]
+    threshold: f32,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // Read xml file
+    let file_string = match std::fs::read_to_string(&args.coverage_file) {
+        Ok(f) => f,
+        Err(e) => panic!("Error reading file: {}", e),
     };
 
-    for patch in patches {
-        println!("patch: {:#?}", patch);
+    // Diff command
+    let cmd = String::from("git diff ") + &args.branch;
+    let diff_file_string = run_command(&cmd);
+
+    // Parse diff file
+    let diff_files = DiffFiles::new(&diff_file_string);
+    let coverage: Coverage = Coverage::new(&file_string);
+
+    println!("Coverage file: {}", args.coverage_file);
+    println!("Count of files changed: {}", diff_files.files.len());
+
+    let mut total_lines_changed = 0;
+    let mut total_lines_covered = 0;
+    for file in diff_files.files {
+        let file_path = file.0;
+
+        // Check if the file is a code file
+        if !(file_path.ends_with(".rs") || file_path.ends_with(".py")) {
+            continue;
+        }
+
+        let lines_changed = file.1;
+        let lines_covered = coverage.get_lines_covered(file_path.as_str());
+
+        // Count the lines covered out the lines changed
+        let mut lines_covered_count = 0;
+        for line in &lines_changed {
+            if lines_covered.contains(&line) {
+                lines_covered_count += 1;
+            }
+        }
+
+        total_lines_changed += lines_changed.len();
+        total_lines_covered += lines_covered_count;
+
+        println!("File: {}", file_path);
+        println!("Lines changed: {:?}", lines_changed);
+        println!("Lines covered: {:?}", lines_covered);
     }
 
-    // Parse xml file
-    let coverage: Coverage = serde_xml_rs::from_str(&file_string).unwrap();
+    let coverage_percentage = total_lines_covered as f32 / total_lines_changed as f32 * 100.0;
 
-    println!("coverage: {:#?}", coverage.version);
-    println!("coverage: {:#?}", coverage.timestamp);
+    println!("Total lines changed: {}", total_lines_changed);
+    println!("Total lines covered: {}", total_lines_covered);
+    println!("Coverage percentage: {:.2}%", coverage_percentage);
+}
 
-    for package in coverage.packages.list_of_packages {
-        let file_path = package.get_filepath();
-        let lines_covered = package.get_lines_covered();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        println!("Filepath: {:#?}", file_path);
-        println!("Lines covered: {:#?}", lines_covered);
+    #[test]
+    fn test_parse_diff_file() {
+        // Read diff file from assets/diff_files/coverage.diff
+        let diff_file_string = std::fs::read_to_string("assets/diff_files/coverage.diff").unwrap();
     }
 }
