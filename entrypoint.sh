7a923606ebabed3f9185ff555499f989ed181034
Dennis Jensen <ngh1adj95@hotmail.com>
2023-05-06 11:20:41 +0200

    Feature: Added fail on total flag
    Changes:
    - It will only fail on total if prompted to do so
    - Default it will be off in the github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,14 @@
 echo "Running with coverage file $1"
 echo "Comparing with branch $2"
 echo "Using threshold $3%"
+echo "Failing on total coverage below threshold: $4"
 
-/coverage_scope --coverage-file $GITHUB_WORKSPACE/$1 --branch $2 --threshold $3 --git-dir $GITHUB_WORKSPACE
+# Lower case argument 4 and check if it is true
+fail_on_total_arg=$(echo "$4" | tr '[:upper:]' '[:lower:]')
+if [ "$fail_on_total_arg" = "true" ]; then
+    fail_on_total="--fail-on-total"
+else
+    fail_on_total=""
+fi
+
+/coverage_scope --coverage-file $GITHUB_WORKSPACE/$1 --branch $2 --threshold $3 --git-dir $GITHUB_WORKSPACE $fail_on_total
--- src/main.rs
+++ src/main.rs
@@ -40,26 +40,33 @@ struct Args {
     /// If not specified, the current directory is used
     #[arg(short, long)]
     git_dir: Option<String>,
+
+    /// Fail on total coverage below threshold
+    #[arg(short, long)]
+    fail_on_total: bool,
+}
+
+fn change_directory(dir: Option<String>) {
+    // Check if dir exists
+    if dir.is_some() {
+        let git_dir = dir.unwrap();
+        if !std::path::Path::new(&git_dir).exists() {
+            panic!("Directory does not exist: {git_dir}");
+        }
+
+        std::env::set_current_dir(git_dir.clone())
+            .expect(format!("Error changing directory to {git_dir}").as_str());
+    }
 }
 
 fn main() {
     let args = Args::parse();
 
-    // Read xml file
-    let file_string = match std::fs::read_to_string(&args.coverage_file) {
-        Ok(f) => f,
-        Err(e) => panic!("Error reading file {}: {}", args.coverage_file, e),
-    };
+    // Format string
+    let file_string = std::fs::read_to_string(&args.coverage_file)
+        .expect(format!("Error reading file {}", args.coverage_file).as_str());
 
-    if args.git_dir.is_some() {
-        // Check if dir exists
-        let git_dir = args.git_dir.unwrap();
-        if !std::path::Path::new(&git_dir).exists() {
-            panic!("Directory does not exist: {git_dir}");
-        }
-
-        std::env::set_current_dir(&git_dir).unwrap();
-    }
+    change_directory(args.git_dir);
 
     // Diff command
     let cmd = String::from("git diff ") + &args.branch + " --diff-filter=d";
@@ -68,29 +75,42 @@ fn main() {
 
     // Parse diff file
     let coverage: Coverage = Coverage::new(&file_string);
+    let mut threshold_not_met = false;
 
     if !diff_file_string.is_empty() {
         let diff_files = DiffFiles::new(&diff_file_string);
         let line_coverage_percentage = diff_files.calculate_line_coverage(coverage.clone());
-        println!("Git diff coverage is {line_coverage_percentage:.2}%");
+        println!("Current changes coverage is {line_coverage_percentage:.2}%");
 
         if line_coverage_percentage < args.threshold {
             println!(
-                "Git diff coverage is below threshold of {:.2}%",
+                "Current changes coverage is below threshold of {:.2}%",
                 args.threshold
             );
-            std::process::exit(1);
+            threshold_not_met = true;
         }
     }
 
     let total_coverage_percentage = coverage.get_total_coverage();
     println!("Total coverage is {total_coverage_percentage:.2}%");
 
-    if total_coverage_percentage < args.threshold as f64 {
+    if args.fail_on_total && total_coverage_percentage < args.threshold as f64 {
         println!(
             "Total coverage is below threshold of {:.2}%",
             args.threshold
         );
+        threshold_not_met = true;
+    }
+
+    if threshold_not_met {
         std::process::exit(1);
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_lines_covered() {}
 }
