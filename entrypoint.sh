94f382b48f0e9de07e30e58f09efd5a614ba99d2
Dennis Jensen <ngh1adj95@hotmail.com>
2023-05-08 19:05:40 +0200

    Feature: Abstracted the CI jobs
    Changes:
    - Tried to abstract the CI jobs in a more understandable fashion

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo "Comparing with branch $2"
 echo "Using threshold for change: $3%"
 echo "using threshold for total: $4%"
 
-/coverage_scope --coverage-file $GITHUB_WORKSPACE/$1 --branch $2 --threshold-change $3 --threshold-total $4 --git-dir $GITHUB_WORKSPACE
+/coverage_scope --coverage-file $GITHUB_WORKSPACE/$1 --branch $2 --threshold-change $3 --threshold-total $4 --git-dir $GITHUB_WORKSPACE --docker
